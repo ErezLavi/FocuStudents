32deab251296bc1cc5c5edf7057b315a977689b5
erez <Erez1410@gmail.com>
2023-11-29 23:08:14 +0200

    solve throttled timer

--- src/components/timer/timerForm/timerUtils.tsx
+++ src/components/timer/timerForm/timerUtils.tsx
@@ -1,30 +1,31 @@
 import { timerActions } from "../../../store/timer-slice";
-import timerAlarm from '../../../sound/switchModeAlarm.mp3'
+import timerAlarm from "../../../sound/switchModeAlarm.mp3";
+import { setInterval } from "worker-timers";
 
-export const startTimer = (dispatch:any, timerState:any) => {
-    return setInterval(() => {
-      if (timerState.isPaused) {
-        return;
-      }
-      if (timerState.secondsLeft === 0) {
-        return switchMode(dispatch, timerState);
-      }
-      dispatch(timerActions.decrement({ secondsLeft: timerState.secondsLeft}));
-    }, 1000);
-  };
-  
-  const switchMode = (dispatch:any, timerState:any) => {
-    const audio = new Audio(timerAlarm);
-    audio.play();
-    const nextMode = timerState.timerMode === "focus" ? "break" : "focus";
-    const nextSeconds =
-      (nextMode === "focus" ? timerState.focusTime : timerState.breakTime) * 60;
-  
-    dispatch(
-      timerActions.updateTimer({
-        ...timerState,
-        timerMode: nextMode,
-        secondsLeft: nextSeconds,
-      })
-    );
-  };
+export const startTimer = (dispatch: any, timerState: any) => {
+  return setInterval(() => {
+    if (timerState.isPaused) {
+      return;
+    }
+    if (timerState.secondsLeft === 0) {
+      return switchMode(dispatch, timerState);
+    }
+    dispatch(timerActions.decrement({ secondsLeft: timerState.secondsLeft }));
+  }, 1000);
+};
+
+const switchMode = (dispatch: any, timerState: any) => {
+  const audio = new Audio(timerAlarm);
+  audio.play();
+  const nextMode = timerState.timerMode === "focus" ? "break" : "focus";
+  const nextSeconds =
+    (nextMode === "focus" ? timerState.focusTime : timerState.breakTime) * 60;
+
+  dispatch(
+    timerActions.updateTimer({
+      ...timerState,
+      timerMode: nextMode,
+      secondsLeft: nextSeconds,
+    })
+  );
+};
