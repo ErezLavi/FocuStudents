7ba69a9521c0dca92b9b33c444d7c00e456912e8
erez <Erez1410@gmail.com>
2024-04-22 16:02:31 +0300

    Firebase auth build changes

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -1,6 +1,5 @@
 import React, { useState } from "react";
 import LoginForm from "../components/auth/LoginForm";
-import AuthDetails from "../components/auth/AuthDetails";
 import TasksLayout from "../components/layout/tasksLayout/TasksLayout";
 
 const LoginPage: React.FC = () => {
@@ -8,7 +7,6 @@ const LoginPage: React.FC = () => {
   return (
     <TasksLayout>
       <LoginForm />
-      <AuthDetails />
     </TasksLayout>
   );
 };
